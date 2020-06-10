3cca82e66aeb159ae96786490820bc8c96878513
Jim Liu <jun.liu@proaimltd.com>
2020-06-10 19:21:50 +0800

    1. 添加JSON配置文件读; 2. 添加NIO中UDPChannel使用.

--- socket-nio/src/test/java/com/jim/socket/nio/channel/NIOFileChannel.java
+++ socket-nio/src/test/java/com/jim/socket/nio/channel/NIOFileChannel.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.net.URISyntaxException;
@@ -14,6 +15,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
 /**
@@ -28,9 +30,58 @@ import java.util.Set;
 public class NIOFileChannel {
 
 	@Test
+	public void readFile() {
+		URL url = Thread.currentThread().getContextClassLoader().getResource("config/config.json");
+		if (url != null) {
+			try (
+					RandomAccessFile file = new RandomAccessFile(new File(url.toURI()), "r");
+					FileChannel fileChannel = file.getChannel();
+			) {
+
+				ByteBuffer byteBuffer = ByteBuffer.allocate(512);
+				//读取文件
+				while (fileChannel.read(byteBuffer) > 0) {
+					byteBuffer.flip();
+					while (byteBuffer.hasRemaining()) {
+						log.info("byteBuffer content = {}", (char) byteBuffer.get());
+					}
+				}
+			} catch (IOException | URISyntaxException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+	@Test
+	public void readFile2() {
+		URL url = Thread.currentThread().getContextClassLoader().getResource("config/config.json");
+		if (url != null) {
+			try (FileInputStream inputStream = new FileInputStream(Paths.get(url.toURI()).toFile());
+				 FileChannel fileChannel = inputStream.getChannel()) {
+				Optional<FileChannel> optionalFileChannel = Optional.ofNullable(fileChannel);
+				optionalFileChannel.ifPresent((f) -> {
+					ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+					try {
+						while (fileChannel.read(byteBuffer) > 0) {
+							byteBuffer.flip();
+							while (byteBuffer.hasRemaining()) {
+								log.info("content = {}", (char) byteBuffer.get());
+							}
+						}
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				});
+			} catch (URISyntaxException | IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	@Test
 	public void writeFile() {
 		URL url = Thread.currentThread().getContextClassLoader().getResource("config/config.json");
-
 		Set<StandardOpenOption> openOptions = new HashSet<>();
 		openOptions.add(StandardOpenOption.WRITE);
 		openOptions.add(StandardOpenOption.APPEND);
@@ -40,7 +91,6 @@ public class NIOFileChannel {
 			fileChannel = FileChannel.open(path, openOptions);
 			ByteBuffer byteBuffer = ByteBuffer.wrap("Hello".getBytes());
 			fileChannel.write(byteBuffer);
-
 		} catch (URISyntaxException | IOException e) {
 			e.printStackTrace();
 		} finally {
@@ -53,42 +103,4 @@ public class NIOFileChannel {
 			}
 		}
 	}
-
-	@Test
-	public void readFile() {
-		URL url = Thread.currentThread().getContextClassLoader().getResource("config/config.json");
-		RandomAccessFile file = null;
-		FileChannel fileChannel = null;
-		try {
-			file = new RandomAccessFile(new File(url.toURI()), "rw");
-			fileChannel = file.getChannel();
-			ByteBuffer byteBuffer = ByteBuffer.allocate(512);
-			//读取文件
-			while (fileChannel.read(byteBuffer) > 0) {
-				byteBuffer.flip();
-				while (byteBuffer.hasRemaining()) {
-					log.info("byteBuffer content = {}", (char) byteBuffer.get());
-				}
-			}
-		} catch (IOException | URISyntaxException e) {
-			e.printStackTrace();
-		} finally {
-			if (fileChannel != null) {
-				try {
-					fileChannel.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-
-			if (file != null) {
-				try {
-					file.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-		}
-	}
-
 }
